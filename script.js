310e66d3a3d856799876db260d88baed29a31e30
elea641 <eleavoliotis64@gmail.com>
2023-03-16 10:11:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+import chalk from 'chalk';
+const wildersName = ["Elea", "Jayson", "Piere", "Eva", "Eric"];
+
+
+console.log(chalk.blue(wildersName[0]));
+console.log(chalk.yellow(wildersName[1]));
+console.log(chalk.red(wildersName[2]));
+console.log(chalk.green(wildersName[3]));
